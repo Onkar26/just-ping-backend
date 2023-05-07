d19c1e5bdc7707d7c0377506e9b58dc6e770e235
unknown <onkarlimkar26@gmail.com>
2023-05-07 13:53:45 +0530

    messages controller completed!!!

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,41 @@
 class MessagesController < ApplicationController
 
     def get_messages
-        render :json => User.all
+        render :json => Message.where("from_user_id = '#{request.headers['From-User-Id']}' or to_user_id = '#{To-User-Id}'")
     end
 
     def send_message
+        message = Message.new(from_user_id: params[:from_user_id], to_user_id: params[:to_user_id], message: params[:message], status: 'active')
+
+        unless message.save
+            render :json => message.errors
+            return
+        end
+
+        render :json => { id: message.id } 
+    end
+
+    def edit_message
+        message = Message.where(id: params[:id]).take
         
+        unless message.present?
+            render :json => { error: 'message is invalid!' } 
+        end
+       
+        unless message.update(message: params[:message])
+            render :json => message.errors
+        end
+
+        render :json => { id: message.id }
     end
 
     def delete_message
-        
+        message = Message.where(id: params[:id]).take
+
+        unless message.update(status: 'active')
+            render :json => message.errors
+        end
+
+        render :json => { id: message.id }
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   get '/users', to: 'users#get_users'
   get '/users/validate_user', to: 'users#validate_user'
   post '/users/add_user', to: 'users#add_user'
+
+  get '/messages/:from_user_id', to: 'messages#get_messages' # of specific user
+  post '/messages/:from_user_id', to: 'messages#send_message' # from - to
+  put '/messages/:from_user_id', to: 'messages#edit_message' 
+  delete '/messages/:id', to: 'messages#delete_message'
 end
